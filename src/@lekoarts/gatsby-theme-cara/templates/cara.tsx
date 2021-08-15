7338a0094290720fe3948843aac6a945f34f8d9f
Olli Suutari <35490459+Olli-Suutari@users.noreply.github.com>
2021-08-16 01:13:51 +0300

    Better scaling + resize sections when window size changes

--- src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
+++ src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
@@ -7,20 +7,49 @@ import About from "../components/about"
 import Contact from "../components/contact"
 
 
-let isMobile = false
+let totalPages = 6.5
+let projectOffset = 1.25
+let aboutOffset = 3
+let contactOffset = 4.5
 if (typeof window !== "undefined") {
-    if (window.innerHeight < 800) {
-        isMobile = true
-    }
+    const adjustSectionHeights = () => {
+        if (window.innerHeight < 650) {
+            totalPages = 11.5
+            projectOffset = 2.5
+            aboutOffset = 7
+            contactOffset = 9
+        }
+        else if (window.innerHeight < 850) {
+            totalPages = 11
+            projectOffset = 2
+            aboutOffset = 6
+            contactOffset = 8
+        }
+        else if (window.innerHeight < 1050) {
+            totalPages = 10
+            projectOffset = 1.9
+            aboutOffset = 4.5
+            contactOffset = 7
+        }
+        else if (window.innerHeight < 1300) {
+            totalPages = 8.5
+            projectOffset = 1.6
+            aboutOffset = 4
+            contactOffset = 6
+        }
+    };
+    // Add event listener for resizing the view.
+    window.addEventListener('resize', adjustSectionHeights)
+    adjustSectionHeights()
 }
 
 const Cara = () => (
     <Layout>
-        <Parallax pages={isMobile ? 11.5 : 6.5}>
+        <Parallax pages={totalPages}>
             <Hero offset={0} />
-            <Projects offset={isMobile ? 2.5 : 1.25} />
-            <About offset={isMobile ? 6.5 : 3} />
-            <Contact offset={isMobile ? 9 : 4.5} />
+            <Projects offset={projectOffset} />
+            <About offset={aboutOffset} />
+            <Contact offset={contactOffset} />
         </Parallax>
     </Layout>
 )
